b12aa9f318aa0b253b7e032792bc7acfae38d90b
Minhyuk Yoon <ymh92730@gmail.com>
2020-12-04 11:29:20 +0900

    [Summary] 1. config-i18n module 추가

--- config-i18n/src/main/java/me/demo/reactive/i18n/config/MessageSourceConfig.java
+++ config-i18n/src/main/java/me/demo/reactive/i18n/config/MessageSourceConfig.java
@@ -1,8 +1,7 @@
-package me.demo.reactive.domainglobal.adapter.config;
+package me.demo.reactive.i18n.config;
 
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.MessageSourceSupport;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.io.Resource;
@@ -11,7 +10,6 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
-@Configuration
 public class MessageSourceConfig {
     @Bean
     public MessageSourceSupport messageSource() {
@@ -24,7 +22,7 @@ public class MessageSourceConfig {
     @Bean
     public ApplicationRunner messageSourceRunner(final ReloadableResourceBundleMessageSource messageSource) {
         return args -> {
-            final String messageSourcesLocationPattern = "classpath:/i18n/*message_*.properties";
+            final String messageSourcesLocationPattern = "classpath*:/i18n/*message_*.properties";
             final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
             for (final Resource resource : resolver.getResources(messageSourcesLocationPattern)) {
                 Optional.ofNullable(resource.getFilename())
